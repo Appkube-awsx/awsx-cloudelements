60f7e66484aff830966dceab02105d8fe4f1394c
ManojSynectiks <manoj.sharma@synectiks.com>
2023-01-12 15:52:24 +0530

    initial commit

---
+++ appconfig/main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2023 Manoj Sharma manoj.sharma@synectiks.com
+*/
+package main
+
+import "github.com/Appkube-awsx/awsx-cloudelements/appconfig/cmd"
+
+func main() {
+	cmd.Execute()
+}
